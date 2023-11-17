00254f98d2d7c3c60227b3c7193f3737578e4fce
pgosar <pgosarsteam@gmail.com>
2023-11-16 19:19:58 -0600

    refactor(style): add rustfmt and reformat accordingly

--- src/seq/client.rs
+++ src/seq/client.rs
@@ -3,45 +3,42 @@ use std::net::TcpStream;
 use std::str::from_utf8;
 
 pub struct Client {
-    ip: String,
-    port: u16,
+  ip: String,
+  port: u16,
 }
 
 impl Client {
-    pub fn new(ip: String, port: u16) -> Client {
-        Client {
-            ip: ip,
-            port: port
-        }
-    }
-    pub fn run_client(&self, msg: String, repeats: i32) -> std::io::Result<()> {
-        let address: String = format!("[{}]:{}", self.ip, self.port);
-        match TcpStream::connect(address) {
-            Ok(mut stream) => {
-                println!("Successfully connected to server in port {}", self.port);
-                let byte_msg = msg.as_bytes();
-                let mut data = [0 as u8; 1024];
-            
-                for _ in 0..repeats {
-                    stream.write(byte_msg)?;
-                    println!("Client Sent: {}", msg);
-                    match stream.read(&mut data) {
-                        Ok(_) => {
-                            println!("Client Received: {}", from_utf8(&data).unwrap());
-                        }
-                        Err(e) => {
-                            println!("Failed to receive data: {}", e);
-                        }
-                    }
-                    std::thread::sleep(std::time::Duration::from_secs(2));
-                }
-                // the size becomes 0 in server.rs when this call finishes because
-                // the connection closes when the listener scope is gone
+  pub fn new(ip: String, port: u16) -> Client {
+    Client { ip: ip, port: port }
+  }
+  pub fn run_client(&self, msg: String, repeats: i32) -> std::io::Result<()> {
+    let address: String = format!("[{}]:{}", self.ip, self.port);
+    match TcpStream::connect(address) {
+      Ok(mut stream) => {
+        println!("Successfully connected to server in port {}", self.port);
+        let byte_msg = msg.as_bytes();
+        let mut data = [0 as u8; 1024];
+
+        for _ in 0..repeats {
+          stream.write(byte_msg)?;
+          println!("Client Sent: {}", msg);
+          match stream.read(&mut data) {
+            Ok(_) => {
+              println!("Client Received: {}", from_utf8(&data).unwrap());
             }
             Err(e) => {
-                println!("Failed to connect: {}", e);
+              println!("Failed to receive data: {}", e);
             }
+          }
+          std::thread::sleep(std::time::Duration::from_secs(2));
         }
-        Ok(())
+        // the size becomes 0 in server.rs when this call finishes because
+        // the connection closes when the listener scope is gone
+      }
+      Err(e) => {
+        println!("Failed to connect: {}", e);
+      }
     }
+    Ok(())
+  }
 }
--- src/seq/server.rs
+++ src/seq/server.rs
@@ -3,65 +3,67 @@ use std::io::prelude::*;
 use std::net::{Shutdown, SocketAddr, TcpListener, TcpStream};
 
 pub struct Server {
-    ip: String,
-    port: u16,
-    key: String,
+  ip: String,
+  port: u16,
+  key: String,
 }
 
 impl Server {
-    pub fn new(ip: String, port: u16, key: String) -> Server {
-        Server {
-            ip: ip,
-            port: port,
-            key: key
-        }
+  pub fn new(ip: String, port: u16, key: String) -> Server {
+    Server {
+      ip: ip,
+      port: port,
+      key: key,
     }
-    pub fn run_server(&self) -> std::io::Result<()> {
-        let sock = Socket::new(Domain::IPV6, Type::STREAM, None).unwrap();
-        sock.set_only_v6(false)?;
-        let address: SocketAddr = format!("[{}]:{}", self.ip, self.port).parse().unwrap();
-        sock.bind(&address.into())?;
-        sock.listen(128)?;
-        let listener: TcpListener = sock.into();
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-                    println!("New connection: {}", stream.peer_addr().unwrap());
-                    self.handle_client(stream);
-                }
-                Err(e) => {
-                    println!("Error: {}", e);
-                }
-            }
+  }
+  pub fn run_server(&self) -> std::io::Result<()> {
+    let sock = Socket::new(Domain::IPV6, Type::STREAM, None).unwrap();
+    sock.set_only_v6(false)?;
+    let address: SocketAddr = format!("[{}]:{}", self.ip, self.port).parse().unwrap();
+    sock.bind(&address.into())?;
+    sock.listen(128)?;
+    let listener: TcpListener = sock.into();
+    for stream in listener.incoming() {
+      match stream {
+        Ok(stream) => {
+          println!("New connection: {}", stream.peer_addr().unwrap());
+          self.handle_client(stream);
         }
-        Ok(())
+        Err(e) => {
+          println!("Error: {}", e);
+        }
+      }
     }
-    
-    pub fn handle_client(&self, mut stream: TcpStream) {
-        println!("handling client");
-        let mut buf = [0; 1024];
-        stream.set_read_timeout(None).expect("set_read_timeout call failed");
+    Ok(())
+  }
 
-        while let Ok(size) = stream.read(&mut buf) {
-            if size == 0 {
-                println!("size is 0");
-                break;
-            }
-            println!("Server Received: {}", String::from_utf8_lossy(&buf[..size]));
-            match stream.write_all(&buf[0..size]) {
-                Ok(_) => {
-                    println!("Server Sent: {}", String::from_utf8_lossy(&buf[..size]));
-                }
-                Err(_) => {
-                    println!(
-                        "An error occurred while writing, terminating connection with {}",
-                        stream.peer_addr().unwrap()
-                    );
-                    stream.shutdown(Shutdown::Both).unwrap();
-                    break;
-                }
-            }
+  pub fn handle_client(&self, mut stream: TcpStream) {
+    println!("handling client");
+    let mut buf = [0; 1024];
+    stream
+      .set_read_timeout(None)
+      .expect("set_read_timeout call failed");
+
+    while let Ok(size) = stream.read(&mut buf) {
+      if size == 0 {
+        println!("size is 0");
+        break;
+      }
+      println!("Server Received: {}", String::from_utf8_lossy(&buf[..size]));
+      match stream.write_all(&buf[0..size]) {
+        Ok(_) => {
+          println!("Server Sent: {}", String::from_utf8_lossy(&buf[..size]));
         }
-        println!("client all done");
+        Err(_) => {
+          println!(
+            "An error occurred while writing, terminating connection with {}",
+            stream.peer_addr().unwrap()
+          );
+          stream.shutdown(Shutdown::Both).unwrap();
+          break;
+        }
+      }
     }
+    println!("client all done");
+  }
 }
