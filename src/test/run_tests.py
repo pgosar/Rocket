c1acb6de748a55cd93898cd7cddf0da0c252aa69
pgosar <pgosarsteam@gmail.com>
2023-12-03 12:50:47 -0600

    fix: correctly terminate process

--- src/test/run_tests.py
+++ src/test/run_tests.py
@@ -8,20 +8,19 @@ def run_command(command):
     num_disconnects = 0
     for i in range(len(command) - 1):
         if command[i] == "-n":
-            num_disconnects = command[i + 1]
+            num_disconnects = int(command[i + 1])
             break
     print(f"Expecting {num_disconnects} disconnect messages.")
     process = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    disconnects = 0
 
     def check_output():
-        nonlocal disconnects
+        disconnects = 0
         for line in process.stdout:
             print(line, end='')
             if "disconnecting client" in line.lower():
                 disconnects += 1
-            if disconnects == num_disconnects:
+            if disconnects >= num_disconnects:
                 print(
                     f"Reached {num_disconnects} disconnect messages. Terminating process.")
                 process.terminate()
